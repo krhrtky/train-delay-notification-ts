e95a1f91d2c0c29bac36f7f2094209e840fb5104
t-kurihara <takuya.kurihara00@gmail.com>
2019-10-13 08:44:07 +0900

    project init.

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  env: { node: true, es6: true },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    semi: ['error', 'never', { beforeStatementContinuationChars: 'never' }],
+    'prettier/prettier': ['error', { singleQuote: true, semi: false }],
+  },
+};
